9fc80220059f8f6eec8d0bb204be21fc0c2499b6
Adam Hull <adam@hmlad.com>
2016-10-03 14:39:00 -0700

    Add LTR roman-to-int

--- leetcode/13-roman-to-int.js
+++ leetcode/13-roman-to-int.js
@@ -19,7 +19,8 @@ const VALUES = {
     'I': 1
 }
 
-var romanToInt = function(s) {
+/* Read right to left */
+var romanToIntRTL = function(s) {
     if(!s || s.length < 1) {
         return 0;
     }
@@ -34,5 +35,31 @@ var romanToInt = function(s) {
         }
         term = values[i];
     }
+    return total;
+};
+
+/* Read left to right, with 1 lookback */
+var romanToInt = function(s) {
+    if(!s || s.length < 1) {
+        return 0;
+    }
+    let total = 0;
+    let prev = Number.POSITIVE_INFINITY;
+    for(let l of s) {
+        let v = VALUES[l];
+        if(v > prev) {
+            total += v - prev;
+            prev = Number.POSITIVE_INFINITY;
+        } else {
+            if(prev != Number.POSITIVE_INFINITY) {
+                total += prev;
+            }
+            prev = v;
+        }
+    }
+    if(prev != Number.POSITIVE_INFINITY) {
+        total += prev;
+    }
+
     return total;
 };
