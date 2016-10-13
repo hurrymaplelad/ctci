20509fd424dc942d62f2600de7a32fa3a2e21cc0
Adam Hull <adam@hmlad.com>
2016-10-13 13:40:03 -0700

    Merge sorted linked lists

---
+++ leetcode/21-merge-sorted-linked-lists.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    if(!(l1 && l2)) return l1 || l2;
+    let head, tail;
+    if(l1.val < l2.val) {
+        head = l1;
+        l1 = l1.next;
+    } else {
+        head = l2;
+        l2 = l2.next;
+    }
+    tail = head;
+
+    while(l1 && l2) {
+        if(l1.val < l2.val) {
+            tail.next = l1;
+            l1 = l1.next;
+        } else {
+            tail.next = l2;
+            l2 = l2.next;
+        }
+        tail = tail.next;
+    }
+    tail.next = l1 || l2;
+    return head;
+};
